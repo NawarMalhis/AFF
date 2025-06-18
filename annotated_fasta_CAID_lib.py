0217d7af8063bd3619913e263328d022e0f5b2a8
nmalhis <nmalhis@gmail.com>
2025-06-18 09:45:08 -0700

    NM Commit PyCharm

--- annotated_fasta_CAID_lib.py
+++ annotated_fasta_CAID_lib.py
@@ -1,8 +1,9 @@
+import numpy as np
 from annotated_fasta import *
 import os
 
 
-def aff_load_prd_caid_scores(af, sc_caid_file, prd):
+def aff_load_prd_merged_caid_scores(af, sc_caid_file, prd):
     with open(sc_caid_file, 'r') as fin:
         ac = ''
         for line in fin:
@@ -31,13 +32,41 @@ def aff_load_prd_caid_scores(af, sc_caid_file, prd):
                 del af['data'][ac]['scores'][prd]
 
 
-def aff_load_caid_scores_single_file(af, scores_path, prd_list, remove_missing_scores=False):
+def load_protein_caid_scores_one(in_file):
+    sc_list = []
+    with open(in_file, 'r') as fin:
+        for line in fin:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            if line[0] == '#':
+                continue
+            if line[0] == '>':
+                ac = line[1:]
+            else:
+                sc_list.append(float(line.split()[2]))
+    return np.array(sc_list, dtype='float32')
+
+
+def aff_load_caid_scores(af, scores_path, prd_list, merged=True, remove_missing_scores=False):
+
     path_files = os.listdir(scores_path)
-    for prd in prd_list:
-        if f"{prd}.caid" not in path_files:
-            print(f"{prd} not found")
-            continue
-        aff_load_prd_caid_scores(af, sc_caid_file=f'{scores_path}{prd}.caid', prd=prd)
+    if merged:
+        for prd in prd_list:
+            if f"{prd}.caid" not in path_files:
+                print(f"{prd} not found")
+                continue
+            aff_load_prd_merged_caid_scores(af, sc_caid_file=f'{scores_path}{prd}.caid', prd=prd)
+    else:
+        for prd in prd_list:
+            _p = f"{scores_path}{prd}/"
+            ac_list = [x[:-5] for x in os.listdir(_p) if x[-5:] == '.caid']
+            for ac in ac_list:
+                if ac not in af['data']:
+                    continue
+                if 'scores' not in af['data'][ac]:
+                    af['data'][ac]['scores'] = {}
+                af['data'][ac]['scores'][prd] = load_protein_caid_scores_one(in_file=f'{_p}{ac}.caid')
 
     used_prd_set = set()
     ac_list = list(af['data'].keys())
